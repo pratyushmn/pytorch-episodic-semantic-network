cc09992724edc978c0ff564c4e55f7ff53861ced
Pratyush Menon <39736182+pratyushmn@users.noreply.github.com>
2022-04-14 00:49:35 -0400

    Cleaning up code
    Cleaning up code

--- main/lib/utils.py
+++ main/lib/utils.py
@@ -1,9 +1,6 @@
 import numpy as np
 import torch
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
 def activatePolicy(policy, tau):
     return np.clip(policy * np.exp(-1 / tau) , 0, 1)
 
@@ -13,9 +10,3 @@ def convertToProbability(x, NewMin, NewMax):
     new_x = (((x - OldMin) * (NewMax - NewMin)) / (OldMax - OldMin)) + NewMin
     probs = new_x / torch.sum(new_x)
     return probs.cpu().numpy()
-
-def convertToProbabilityNew(x, alpha, trialTime):
-    new_alpha = max(alpha - trialTime/4000, 0)
-    new_x = new_alpha*(x/np.sum(x)) + (1 - new_alpha)/8
-
-    return new_x, new_alpha
